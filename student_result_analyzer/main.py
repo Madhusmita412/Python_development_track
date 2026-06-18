7081ee52ea4b80515e4417ed9fede58c071bca88
madhusmita choudhary <juug24btech28036@jainuniversity.ac.in>
2026-06-18 21:17:19 +0530

    made changes

--- student_result_analyzer/main.py
+++ student_result_analyzer/main.py
@@ -2,22 +2,24 @@ students = []
 
 # Take input from user
 num_students = int(input("Enter number of students: "))
+
 for i in range(num_students):
+
     print(f"\nEnter details for Student {i + 1}")
 
-name = input("Enter Name: ")
-math = int(input("Enter Math Marks: "))
-science = int(input("Enter Science Marks: "))
-english = int(input("Enter English Marks: "))
+    name = input("Enter Name: ")
+    math = int(input("Enter Math Marks: "))
+    science = int(input("Enter Science Marks: "))
+    english = int(input("Enter English Marks: "))
 
-student = {
-    "name": name,
-    "math": math,
-    "science": science,
-    "english": english
-}
+    student = {
+        "name": name,
+        "math": math,
+        "science": science,
+        "english": english
+    }
 
-students.append(student)
+    students.append(student)
 
 
 print("\n===== STUDENTS DATA =====")
